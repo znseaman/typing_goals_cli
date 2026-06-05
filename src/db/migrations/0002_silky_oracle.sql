6eedbe722bc8c9904a032c9215429a98afb16239
znseaman <znseaman@gmail.com>
2026-06-04 21:01:06 -0700

    fix: modify migration script from text to json for full_details

--- src/db/migrations/0002_silky_oracle.sql
+++ src/db/migrations/0002_silky_oracle.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "presets" ALTER COLUMN "full_details" SET DATA TYPE json;
+-- ALTER TABLE "presets" ALTER COLUMN "full_details" SET DATA TYPE json;
+ALTER TABLE "presets" ALTER "full_details" TYPE JSON USING to_json("full_details");
